ab3acd3a6d29575271b4c557b7a2efde536e5333
Mansko09 <127218021+Mansko09@users.noreply.github.com>
2025-05-11 17:18:46 +0200

    naming,checking and deleting tasks

--- script.js
+++ script.js
@@ -10,6 +10,38 @@ const classNames = {
   const uncheckedCountSpan = document.getElementById('unchecked-count')
   
   function newTodo() {
-    alert('New TODO button clicked!')
+    const taskName = prompt("Quel est le nom de la tâche ?");
+    const newTask = document.createElement("li");
+    newTask.classList.add(classNames.TODO_ITEM);
+    newTask.textContent = taskName;
+    list.appendChild(newTask);
+    const checkbox = document.createElement("input");
+    checkbox.type = "checkbox";
+    checkbox.classList.add(classNames.TODO_CHECKBOX);
+    newTask.appendChild(checkbox);
+    itemCountSpan.textContent++;
+    uncheckedCountSpan.textContent++;
+    checkbox.addEventListener("change",function(){
+        if (checkbox.checked){
+            uncheckedCountSpan.textContent--;
+        }
+        else{
+            uncheckedCountSpan.textContent++;
+        }
+    })
+    const del= document.createElement("button");
+    del.textContent="X";
+    del.classList.add(classNames.TODO_DELETE);
+    newTask.appendChild(del);
+    del.addEventListener("click",function(){
+        if(!checkbox.checked){
+            uncheckedCountSpan.textContent--;
+        }
+        newTask.remove();
+        itemCountSpan.textContent--;
+    })
+
+
+    // alert('New TODO button clicked!')
   }
   
